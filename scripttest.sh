0d5aac8b57f6e35095019f6d218a3e05c5137c25
Yahav Horev <yahavhorev@gmail.com>
2020-10-05 15:37:41 +0300

    edited

--- scripttest.sh
+++ scripttest.sh
@@ -3,8 +3,4 @@
 
 echo "this is a para script"
 NUMBER=$(($NUMBERONE + $NUMBERTWO))
-echo $NUMBER
-<<<<<<< HEAD
-=======
-echo "yahav"
->>>>>>> 2d827b88bafe2c86f5534f0e5fb2155e123d6172
+echo "its ok"
