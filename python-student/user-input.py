458f7c54a6bb0a86715b1789a1724053b168849c
Yahav Horev <yahavhorev@gmail.com>
2021-01-25 13:55:59 +0200

    learning

---
+++ python-student/user-input.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+
+name = input("What is your name? ")
+birthdate = input("What is your birth of date? ")
+age = int(input("How old are you? "))
+
+print(f"This is your deatails:")
+print(f"Your name is: {name} And you was born in: {birthdate} And you just turn into {age} years old last month")
