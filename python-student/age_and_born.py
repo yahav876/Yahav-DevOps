5bb56bc7ae5ea5cc99e1a2c4b0ecc05e3bc44e9d
Yahav Horev <yahavhorev@gmail.com>
2021-01-25 13:16:14 +0200

    learning

---
+++ python-student/age_and_born.py
@@ -0,0 +1,6 @@
+first_name = 'Yahav'
+last_name = 'Horev'
+birth_date = '7/7/1994'
+
+print(f"My name is: {first_name} {last_name}")
+print(f"And i was born in: {birth_date}")
---
+++ python-student/or.py
@@ -0,0 +1,8 @@
+first = ""
+last = "yahav"
+if first or last:
+    print("You have only last name")
+
+
+last_name = ""
+first_name = "
