411b8739b5df0e7c856365da11f3049037a79ec0
Yahav Horev <yahavhorev@gmail.com>
2021-01-31 14:30:38 +0200

    learning

--- python-student/arguments.py
+++ python-student/arguments.py
@@ -12,16 +12,27 @@ parser.add_argument('--version', '-v', action='version',  version='%(prog)s 1.0'
 # Parse the arguments
 args = parser.parse_args()
 
-with open(args.filename) as f:
-    lines = f.readlines()
-    lines.reverse()
+try:
+    f = open(args.filename)
+    limit = args.limit
+except FileNotFoundError as err:
+    print(f"Error: {err}")
+else:
+    with f:
+        lines = f.readlines()
+        lines.reverse()
 
-    if args.limit:
-        lines = lines[:args.limit]
-
-    for line in lines:
-        print(line.strip()[::-1])
-
+        if args.limit:
+            lines = lines[:args.limit]
 
 # Read the file, reverse the contents and print
+        for line in lines:
+            print(line.strip()[::-1])
+
+# Anyway finally will be printed (with or without err)
+finally:
+    print('Finally')
+
+
+
 
