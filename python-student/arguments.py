0a823c29f0858c4ad1af276e5597005b7592d7b8
Yahav Horev <yahavhorev@gmail.com>
2021-01-31 13:34:27 +0200

    lerning

--- python-student/arguments.py
+++ python-student/arguments.py
@@ -1,5 +1,4 @@
-#!/bin/python3
-
+#!/usr/bin/python3
 import argparse
 
 # Build the parser
@@ -9,10 +8,20 @@ parser.add_argument('filename', help='the file to read')
 parser.add_argument('--limit', '-l', type=int, help='The number of lines to read')
 parser.add_argument('--version', '-v', action='version',  version='%(prog)s 1.0')
 
-args = parser.parse_args()
-print(args)
 
 # Parse the arguments
+args = parser.parse_args()
+
+with open(args.filename) as f:
+    lines = f.readlines()
+    lines.reverse()
+
+    if args.limit:
+        lines = lines[:args.limit]
+
+    for line in lines:
+        print(line.strip()[::-1])
+
 
 # Read the file, reverse the contents and print
 
