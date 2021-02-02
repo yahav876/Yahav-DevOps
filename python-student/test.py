5bab806c603a4012e2f2a3e5245816ae26196a6f
Yahav Horev <yahavhorev@gmail.com>
2021-02-02 10:55:43 +0200

    learning

--- python-student/test.py
+++
@@ -1,4 +0,0 @@
-#!/bin/python3
-print("two plus two is " + str(2+2))
-
-
--- python-student/working-with-if-else.py
+++ python-student/working-with-if-else.py
@@ -2,13 +2,18 @@
 
 # Exercise: Working with If/Else
 
-user = {'admin': True, 'active': False, 'name': 'yahav'}
+users = [
+    {'admin': True, 'active': True, 'name': 'yahav'},
+    {'admin': True, 'active': True, 'name': 'shlomi'},
+    {'admin': True, 'active': True, 'name': 'hen'},
+    {'admin': True, 'active': True, 'name': 'dor'},
+    ]
 
-if user['admin'] and user['active']:
-    prefix = "ACTIVE -(ADMIN)"
-elif user['admin']:
-    prefix = "(ADMIN)"
-elif user['active']:
-    prefix = "ACTIVE"
+line = 1
 
-print(prefix + user['name']) 
+for user in users:
+    prefix = f"{line} "
+
+    if user['admin'] and user['active']:
+        prefix += "Active - (ADMIN)"
+    print(prefix + user['name'])
