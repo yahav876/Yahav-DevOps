81094949af7aba51508bf7d9a0522b9dc6018eb8
yahav876 <ya94$hav>
2021-06-02 11:19:00 +0300

    wordpress

--- terraform-student/wordpress-infrastrcture/cloudfront.tf
+++ terraform-student/wordpress-infrastrcture/cloudfront.tf
@@ -6,7 +6,6 @@ variable "bucket-name" {
 resource "aws_s3_bucket" "mdeia-code" {
 #  provider = var.region-master
   count    = var.buckets-num["bucket"] == [""] ? 2 : 0
-  bucket   = element(var.bucket-name, count.index)
+  bucket   = "${var.bucket-name[count.index]}"
   acl      = "public-read"
-
-}
+  }
