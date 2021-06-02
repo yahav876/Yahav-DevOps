969f127e94e79e1cf4f367e689b10f88a463fc4c
yahav876 <ya94$hav>
2021-06-02 10:46:10 +0300

    wordpress

--- terraform-student/wordpress-infrastrcture/providers.tf
+++ terraform-student/wordpress-infrastrcture/providers.tf
@@ -1,7 +1,12 @@
 terraform {
   required_version = ">=0.14.8"
   backend "s3" {
+    region = "us-east-1"
+    profile = "default"
+    key = "terraform-yahav-state"
+    bucket = "terraform-yahav"
   }
+}
    required_providers {
     aws = {
       version = ">= 3.29.1"
@@ -14,4 +19,3 @@ terraform {
         version = ">=2.1.2"
       }
     }
-}
