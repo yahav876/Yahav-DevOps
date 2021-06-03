634d968fb4d7baa8e45e626ddf0ddeb33091bb94
yahav876 <ya94$hav>
2021-06-03 15:39:03 +0300

    wordpress

--- terraform-student/wordpress-infrastrcture/modules/main.tf
+++ terraform-student/wordpress-infrastrcture/modules/main.tf
@@ -1,8 +1,26 @@
+
 provider "aws" {
-  region = local.aws-region
+  region = "us-east-1"
+}
+data "aws_ssm_parameter" "linuxAmi" {
+  provider = aws
+  name     = "/aws/service/ami-amazon-linux-latest/amzn2-ami-hvm-x86_64-gp2"
 }
 
 resource "aws_instance" "wordpress" {
   ami = data.aws_ssm_parameter.linuxAmi.value
   instance_type = "t2.micro"
+  user_data = <<-EOF
+        #!/bin/bash
+        yum update -y
+        yum install httpd
+        cd /var/www/html
+        echo "yahav wordpress" > index.html
+        service httpd start
+        chkconfig httpd on
+        EOF
+
+  tags = {
+    Name  = "Wordpress"
+  }
 }
