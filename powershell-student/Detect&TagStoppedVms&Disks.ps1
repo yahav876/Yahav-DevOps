7a15c41390ee2262dee1d2140d4fac2c43a4ec35
yahav876 <ya94$hav>
2021-06-14 17:54:24 +0300

    wordpress

--- powershell-student/Detect&TagStoppedVms&Disks.ps1
+++ powershell-student/Detect&TagStoppedVms&Disks.ps1
@@ -49,7 +49,7 @@ try {
                     }
                 }
             }         
-    
+    #
           
             
      $orphanDisks
--- terraform-student/wordpress-infrastrcture/modules/user-data.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-yum update -y
-yum install httpd -y
-cd /var/www/html
-echo "yahav wordpress" > index.html
-chkconfig httpd on
-service httpd startupdate -y
