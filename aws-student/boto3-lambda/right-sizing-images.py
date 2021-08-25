deffcf56dbed6e6ff204ad6278043c1b282d03c5
yahav876 <ya94$hav>
2021-08-25 10:06:55 +0300

    right sizing aws images

--- aws-student/boto3-lambda/right-sizing-images.py
+++ aws-student/boto3-lambda/right-sizing-images.py
@@ -10,20 +10,20 @@ SIZE = 128, 128
 
 def lambda_handler(event, context):
     for record in event['Records']:
-        sourcce_bucket = record['s3']['bucket']['name']
+        source_bucket = record['s3']['bucket']['name']
         key = record['s3']['object']['key']
         thumb = 'thumb-' + key
         with tempfile.TemporaryDirectory() as tmpdir:
-            download_path = os.environ.join(tmpdir, key)
+            download_path = os.path.join(tmpdir, key)
             upload_path = os.path.join(tmpdir, thumb)
-            s3.download_file(sourcce_bucket, key, download_path)
+            s3.download_file(source_bucket, key, download_path)
             generate_thumbnail(download_path, upload_path)
             s3.upload_file(upload_path, DEST_BUCKET, thumb)
 
-        print('Thumbnail image saved at {}/{}').format(DEST_BUCKET, thumb)
+        print('Thumbnail image saved at {}/{}'.format(DEST_BUCKET, thumb))
 
 def generate_thumbnail(source_path, dest_path):
     print("Generating thumbnail from:", source_path)
     with Image.open(source_path) as image:
-        image.thumbnail(SIZE)
-        image.save(dest_path)
+            image.thumbnail(SIZE)
+            image.save(dest_path)
