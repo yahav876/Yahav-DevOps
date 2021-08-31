33b176acbf96fca008a23325fa9d10fceb5f6c2e
yahav876 <ya94$hav>
2021-08-31 08:49:02 +0300

    aws lambda

--- aws-student/boto3-lambda/sqs-messages.py
+++ aws-student/boto3-lambda/sqs-messages.py
@@ -3,7 +3,6 @@ import os
 import json
 from datetime import datetime
 
-
 TABLE_NAME = os.environ['TABLE_NAME']
 QUEUE_NAME = os.environ['QUEUE_NAME']
 MAX_QUEUE_MESSAGES = os.environ['MAX_QUEUE_MESSAGES']
@@ -11,33 +10,33 @@ MAX_QUEUE_MESSAGES = os.environ['MAX_QUEUE_MESSAGES']
 sqs = boto3.resource('sqs')
 dynamodb = boto3.resource('dynamodb')
 
-table = dynamodb.Table['TABLE_NAME']
 
 def lambda_handler(event, context):
-
     # Get queue by name and Receive the messages.
     queue = sqs.get_queue_by_name(QueueName=QUEUE_NAME)
 
     print("ApproximateNumberOfMessages:", queue.attributes.get('ApproximateNumberOfMessages'))
 
-    for message in queue.received_message(MaxNumberOfMessages=int(MAX_QUEUE_MESSAGES)):
+    for message in queue.receive_messages(MaxNumberOfMessages=int(MAX_QUEUE_MESSAGES)):
         print(message)
+        table = dynamodb.Table(TABLE_NAME)
 
-    insert = table.put_item(item={
-        'MessageId': message.message_id,
-        'Body': message.body,
-        'Timestamp': datetime.now().isoformat()
-        })
+        # Write the message to dynamoDB
+        response = table.put_item(
+            Item={
+                'MessageId': message.message_id,
+                'Body': message.body,
+                'Timestamp': datetime.now().isoformat()
+            })
 
-    print("Wrote message to dynamoDB table: ", json.dumps(insert))
+        print("Wrote message to dynamoDB table: ", json.dumps(insert))
 
-    # Delete The SQS message
-    message.delete()
-    print("Deleting Message from queue" + message.message_id)
+        # Delete The SQS message
+        message.delete()
+        print("Deleting Message from queue" + message.message_id)
     # for record in event['Records']:
     #     que_message = record['sqs']['message']['body']
     #     message_db = json.dumps(que_message)
     #     insert = table.put_item(item=message_db)
     #     insert_message = dynamodb.put_item(table=TABLE_NAME, item=message_db)
-        
-        
+
