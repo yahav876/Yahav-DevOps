80dd8b5afb131c25494d089a79286fdf08857721
yahav876 <ya94$hav>
2021-09-01 10:10:14 +0300

    azure

---
+++ azure-student/powershell/remediation_auto.ps1
@@ -0,0 +1,53 @@
+######################################################################################################################
+
+#  Copyright 2021 CloudTeam & CloudHiro Inc. or its affiliates. All Rights Reserved.                                 #
+
+#  You may not use this file except in compliance with the License.                                                  #
+
+#  https://www.cloudhiro.com/AWS/TermsOfUse.php                                                                      #
+
+#  This file is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES                                                  #
+
+#  OR CONDITIONS OF ANY KIND, express or implied. See the License for the specific language governing permissions    #
+
+#  and limitations under the License.                                                                                #
+
+######################################################################################################################
+
+PARAM(
+    [string] $SubscriptionNamePattern = '.*',
+    [string] $ConnectionName = 'AzureRunAsConnection',
+    [string] $policyAssignmentId = "",
+    [string] $subscriptionName = ""
+
+)
+
+
+Write-Output ('{0:yyyy-MM-dd HH:mm:ss.f} - Starting' -f (Get-Date))
+
+try {
+
+
+    # Login to Azure
+    if ($env:AUTOMATION_ASSET_ACCOUNTID) {
+        $runAsConnection = Get-AutomationConnection -Name $ConnectionName -ErrorAction Stop
+        Add-AzAccount -ServicePrincipal -Tenant $runAsConnection.TenantId -ApplicationId $runAsConnection.ApplicationId `
+            -CertificateThumbprint $runAsConnection.CertificateThumbprint -ErrorAction Stop | Out-Null
+    }
+    # Iterate all subscriptions
+    # Get-AzSubscription | Where-Object { ($_.Name -match $SubscriptionNamePattern) -and ($_.State -eq 'Enabled') } | ForEach-Object {
+
+        Set-AzContext -SubscriptionName $subscriptionName -Force | Out-Null
+
+        while($(Get-AzPolicyState | Where-Object {$_.PolicyAssignmentId -eq $policyAssignmentId -and $_.ComplianceState -eq "NonCompliant"}))
+        {
+            $job = Start-AzPolicyRemediation -PolicyAssignmentId $policyAssignmentId -Name "remediation$(Get-Random)" -AsJob
+            $job | Wait-Job
+            $remediation = $job | Receive-Job
+        }
+    
+} catch {
+    Write-Output ($_)
+} finally {
+    Write-Output ('{0:yyyy-MM-dd HH:mm:ss.f} - Completed' -f (Get-Date))
+}
