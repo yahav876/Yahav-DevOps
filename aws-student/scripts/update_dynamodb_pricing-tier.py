798a5d1f38f79de014c5279786ed638feb3a504e
yahav876 <ya94$hav>
2021-11-16 09:25:52 +0200

    fix

--- aws-student/scripts/update_dynamodb_pricing-tier.py
+++ aws-student/scripts/update_dynamodb_pricing-tier.py
@@ -1,19 +1,28 @@
+from logging import Filter
 import boto3
 
+dynamodb = boto3.client('dynamodb', region_name='us-east-2')
 
 def lambda_handler(event, context):
 
-    dynamodb = boto3.client('dynamodb', region_name='us-east-2')
 
     tables = dynamodb.list_tables()['TableNames']
-
+    
     for t in tables:
-        dynamodb.table.update(
-            AttributeDefinitions=[
-                {
-                    'AttributeName': t,
-                    'AttributeType': 'S'
-                },
-            ],
-            BillingMode='PAY_PER_REQUEST'
-        )
+
+        try:
+            response = dynamodb.update_table(
+                TableName=t,
+                BillingMode='PAY_PER_REQUEST'
+            )
+            
+        except dynamodb.exceptions.ResourceInUseException as e:
+            pass
+
+while True:
+    second_list = dynamodb.list_tables(ExclusiveStartTableName='prod_6365a174-2aee-40b7-a63c-b4a57e5c3984')['TableNames']
+    for t2 in second_list:
+        response2 = dynamodb.update_table(
+                TableName=t2,
+                BillingMode='PAY_PER_REQUEST'
+                )
