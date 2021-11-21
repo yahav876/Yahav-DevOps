6e983c24861f14d66327851b8b922ea53557d725
yahav876 <ya94$hav>
2021-11-21 15:09:50 +0200

    fix terraform

--- terraform-student/prod-env/ec2/circles/variables.tfvars
+++ terraform-student/prod-env/ec2/circles/variables.tfvars
@@ -3,5 +3,7 @@ general_config = {
     region = "us-east-1" 
     backend_region = "eu-west-3"
     ec2_size = "t2.micro"
+    s3_bucket_name = "circles-up-elb-for-logs"
+    
 
 }
--- terraform-student/prod-env/ec2/src/variables.tf
+++ terraform-student/prod-env/ec2/src/variables.tf
@@ -1,8 +1,13 @@
 variable "general_config" {
-    type = map
-    default = {
-        region = ""
-        backend_region = ""
-        ec2_size = ""
-    }
+  type = map(any)
+  default = {
+    region         = ""
+    backend_region = ""
+    ec2_size       = ""
+    s3_bucket_name = ""
+    launch_template_name = ""
+    
+
+
+  }
 }
