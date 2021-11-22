b7eb134c477d15e6ed0043b0d035f47a8a4edef5
yahav876 <ya94$hav>
2021-11-22 17:43:55 +0200

    fix

--- terraform-student/backend.tf
+++
@@ -1,9 +0,0 @@
-terraform {
-  required_version = ">=0.12.0"
-  backend "s3" {
-    region  = "us-east-1"
-    profile = "default"
-    key     = "terraformstatefile"
-    bucket  = "terraform-state876"
-  }
-}
