498660c735769ca66edc66087d7b1e96884b2ac9
yahav-gitlab <yahav.horev@vidaa.com>
2022-09-06 16:48:22 +0300

    fix

--- bash-scripts/aws-cli/export_route53_zoneFile.sh
+++ bash-scripts/aws-cli/export_route53_zoneFile.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-profile="2019"
-zonename="vidaa.com"
+profile="2017"
+zonename="vidaahub.com"
 hostedzoneid=$(aws route53 list-hosted-zones --output json --profile=$profile | jq -r ".HostedZones[] | select(.Name == \"$zonename.\") | .Id" | cut -d'/' -f3)
-aws route53  list-resource-record-sets --hosted-zone-id $hostedzoneid --output json --profile=$profile | jq -jr '.ResourceRecordSets[] | "\(.Name) \t\(.TTL) \t\(.Type) \t\(.ResourceRecords[]?.Value)\n"' > vidaa-com-records.txt
+aws route53  list-resource-record-sets --hosted-zone-id $hostedzoneid --output json --profile=$profile | jq -jr '.ResourceRecordSets[] | "\(.Name) \t\(.TTL) \t\(.Type) \t\(.ResourceRecords[]?.Value)\n"' > vidaahub-com-records.txt
